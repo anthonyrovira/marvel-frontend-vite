f302cb6ba66fa581f623249db9dc11a2844e3679
Anthony <anthonyrov@gmail.com>
2025-04-09 18:58:59 +0200

    Update package.json to include @types/node dependency and modify vite.config.ts to load environment variables for backend URL configuration in proxy settings.

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,19 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api-reacteur": {
-        target: "https://lereacteur-marvel-api.herokuapp.com",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api-reacteur/, ""),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api-reacteur": {
+          target: env.VITE_REACTEUR_BACKEND_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api-reacteur/, ""),
+        },
       },
     },
-  },
+  };
 });
